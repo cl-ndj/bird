3063232579dd545ba3782e14a1e4cda8ee863b8a
cl-ndj <2586361056@qq.com>
2024-11-27 10:20:47 +0800

    11.27

---
+++ 小鸟案例/Assets/Scripts/Game.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public enum Game_Status
+    {
+        Ready,
+        InGame,
+        GameOver
+
+
+    }
+    private Game_Status status;
+    public GameObject PanleReady;
+    public GameObject PanleInGame;
+    public GameObject PanleGameOver;
+    public PipelineManager pipelineManager;
+    public player player;
+    public Text scoreText;
+    public Text scoreText2;
+    public int score;
+
+    private int Score
+    {
+        get { return score; }
+        set 
+        {
+            score = value;
+            scoreText.text = score.ToString(); 
+            scoreText2.text = score.ToString(); 
+        }
+    }
+
+    private Game_Status Status
+    {
+        get { return status; }
+        set { status = value; UI(); }
+
+    }
+    void Start()
+    {
+        PanleReady.SetActive(true);
+        player.OnDeth += Player_OnDeth;
+        player.scoreAction = getScore;
+    }
+
+    private void getScore(int score)
+    {
+        Score += score;
+    }
+    private void Player_OnDeth()
+    {
+        Status = Game_Status.GameOver;
+        pipelineManager.pileStop();
+      
+
+
+    }
+
+    void Update()
+    {
+        
+    }
+    //游戏开始
+    public void StartGame ()
+    {
+        this.Status = Game_Status.InGame;
+       
+          
+        StartCoroutine(StartGenerationPipes());
+       
+        
+        player.Fly();
+        
+
+    }
+
+    public void GameReset()
+    {
+        Status = Game_Status.Ready;
+        pipelineManager.init();
+        player.Init();
+        Score = 0;
+    }
+    IEnumerator StartGenerationPipes()
+    { 
+        
+        yield return new WaitForSeconds(2.5f);
+        pipelineManager.pileStart();
+
+
+    }
+    public void UI()
+    {
+        PanleReady.SetActive(this.Status == Game_Status.Ready);
+        PanleInGame.SetActive(this.Status == Game_Status.InGame);
+        PanleGameOver.SetActive(this.Status == Game_Status.GameOver);
+
+    }
+}
---
+++ 小鸟案例/Assets/Scripts/player.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class player : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody2D;
+    public float upforce = 100;
+    public float downforce = 4;
+    public float slowforce = 100;
+
+    private Animator animator;
+    public bool death = false;
+    public delegate void deathNotify();
+    public event deathNotify OnDeth;
+    private Vector3 initps;
+    public UnityAction<int> scoreAction;
+    private bool isShift;
+   
+
+    void Start()
+    {
+
+        initps = transform.position;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        Ldie();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.death)
+            return;
+        isShift = Input.GetKey(KeyCode.LeftShift);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isShift)
+            {
+                rigidbody2D.velocity = Vector2.zero;
+                rigidbody2D.AddForce(new Vector2(0, slowforce), ForceMode2D.Force);
+            }
+            else
+            {
+                rigidbody2D.velocity = Vector2.zero;
+                rigidbody2D.AddForce(new Vector2(0, upforce), ForceMode2D.Force);
+            }
+            
+            
+        }
+         if (Input.GetMouseButtonDown(1))
+        {
+            Flydown();
+            rigidbody2D.velocity = Vector2.zero;
+            rigidbody2D.AddForce(new Vector2(0, -downforce), ForceMode2D.Force);
+        }
+       
+
+
+    }
+
+    public void Init()
+    {
+        transform.position = initps;
+        Ldie();
+        death = false;
+    }
+    public void Ldie()
+    {
+        rigidbody2D.Sleep();
+        rigidbody2D.simulated=false;
+        animator.SetTrigger("ldie");
+    }
+    public void Fly()
+    {
+        rigidbody2D.WakeUp();
+
+        rigidbody2D.simulated=true;
+
+        animator.SetTrigger("fly");
+
+    }
+    public void Flydown()
+    {
+        animator.SetTrigger("flydown");
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+      
+        this.death = true;
+        if (OnDeth!=null)
+        {
+            OnDeth();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+      
+        if (scoreAction!=null)
+        
+            scoreAction(1);
+        
+    }
+    
+
+}
